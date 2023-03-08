ecc575704e9dfa5f8ba611a8b02a4ce123048c92
Lattexshz <lattex.code@gmail.com>
2023-03-08 19:40:31 +0900

    Create point method

--- examples/png.rs
+++ examples/png.rs
@@ -5,12 +5,49 @@ use std::io::BufWriter;
 use immo::png::Png;
 
 fn main() {
-    let png = Png::new(500,500);
+    let mut png = Png::new(10,10);
+
+    // Y = 0
+    png.point(4,0,(255,255,255,255));
+    png.point(5,0,(255,255,255,255));
+    // Y = 1
+    png.point(3,1,(255,255,255,255));
+    png.point(6,1,(255,255,255,255));
+    // Y = 2
+    png.point(3,2,(255,255,255,255));
+    png.point(6,2,(255,255,255,255));
+    // Y = 3
+    png.point(2,3,(255,255,255,255));
+    png.point(7,3,(255,255,255,255));
+    // Y = 4
+    png.point(2,4,(255,255,255,255));
+    png.point(7,4,(255,255,255,255));
+    // Y = 5
+    png.point(2,5,(255,255,255,255));
+    png.point(3,5,(255,255,255,255));
+    png.point(4,5,(255,255,255,255));
+    png.point(5,5,(255,255,255,255));
+    png.point(6,5,(255,255,255,255));
+    png.point(7,5,(255,255,255,255));
+    // Y = 6
+    png.point(2,6,(255,255,255,255));
+    png.point(7,6,(255,255,255,255));
+    // Y = 7
+    png.point(1,7,(255,255,255,255));
+    png.point(8,7,(255,255,255,255));
+    // Y = 8
+    png.point(1,8,(255,255,255,255));
+    png.point(8,8,(255,255,255,255));
+    // Y = 9
+    png.point(1,9,(255,255,255,255));
+    png.point(8,9,(255,255,255,255));
+
+
     let path = String::from("sample.png");
     let file = File::create(path).unwrap();
     let w = &mut BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(w, 500,500); // Width is 2 pixels and height is 1.
+    let mut encoder = png::Encoder::new(w, 10,10); // Width is 2 pixels and height is 1.
     encoder.set_color(png::ColorType::Rgba);
     encoder.set_depth(png::BitDepth::Eight);
     // Adding text chunks to the header
--- src/png.rs
+++ src/png.rs
@@ -1,4 +1,4 @@
-pub struct Png(Vec<u8>);
+pub struct Png(Vec<u8>,u32,u32);
 
 impl Png {
     pub fn new(width:u32,height:u32) -> Self {
@@ -10,8 +10,23 @@ impl Png {
             data.push(0);
         }
         Self {
-            0: data
+            0: data,
+            1: width,
+            2: height,
         }
+    }
+
+    pub fn point(&mut self,x:u32,y:u32,color: (u8,u8,u8,u8)) {
+        let index = if x == 0 {
+            (x*4+(y*self.2)*4) as usize
+        }else {
+            (x*4+(y*self.2)*4) as usize
+        };
+        println!("index: {} len: {}",index,self.0.len());
+        self.0[index] = color.0;
+        self.0[index+1] = color.1;
+        self.0[index+2] = color.2;
+        self.0[index+3] = color.3;
     }
 
     pub fn as_slice(&self) -> &[u8] {
