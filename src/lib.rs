1c62b7c5017eeede47be6d96b91cee989c3d7f2d
Lattexshz <lattex.code@gmail.com>
2023-03-10 07:36:44 +0900

    Create sample page

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,17 @@ extern crate log;
 
 pub mod png;
 pub mod error;
+pub mod wasm;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
